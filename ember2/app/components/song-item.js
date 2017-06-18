b7d788bc67dee9883aa5519dcd7339e94efcdd28
Daler Asrorov <asrorids@gmail.com>
2017-06-18 00:21:05 -0700

    removed junk project

--- ember2/app/components/song-item.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  tagName: 'li',
-  isExplicit: Ember.computed('song.trackExplicitness', function() {
-    return this.get('song.trackExplicitness') === 'explicit';
-  })
-});
